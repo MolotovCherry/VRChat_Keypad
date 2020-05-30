0cbacde41d43af2cde1e77528f90ec9346ff1d26
Cherryleafroad <cherryleafroad@gmail.com>
2020-05-29 19:47:09 -0700

    Added KeyPressProxy() to use in custom methods, such as a UI keypad (OnMouseDown doesn't work as a callback in UI code)

--- Keypad/Keypad_Udon_Sharp/Keypad_Button_Desktop.cs
+++ Keypad/Keypad_Udon_Sharp/Keypad_Button_Desktop.cs
@@ -17,4 +17,14 @@ public class Keypad_Button_Desktop : UdonSharpBehaviour
         targetProgram.SetProgramVariable("inputKey", value);
         targetProgram.SendCustomEvent("keyPressed");
     }
+
+    public void KeyPressProxy()
+    {
+        // if you are using UI code buttons, sending a UI button OnClick()
+        // event to UdonBehavior.SendCustomEvent of the same object, using OnMouseDown
+        // as the function, doesn't work.
+        //
+        // This method is here to be used as a proxy for the UI buttons or any other use-case
+        OnMouseDown();
+    }
 }
